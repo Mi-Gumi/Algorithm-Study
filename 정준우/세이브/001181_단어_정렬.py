22988d1029059e622a62d064b49a51db6847e6b4
Mi-Gumi <woojunjung18@gmail.com>
2023-01-26 21:08:26 +0900

    save

--- 정준우/세이브/001181_단어_정렬.py
+++ 정준우/세이브/001181_단어_정렬.py
@@ -5,10 +5,11 @@ word_list = list()
 for cycle in range(num):
     word_list.append(input())
 
+# set 를 통해 중복 제거, 사전 순서대로 정렬 후 길이에 따라 정렬
 compressed_list = list(set(word_list))
-compressed_list.sort(key = lambda x: len(x))
+compressed_list.sort()
+compressed_list.sort(key = len)
 
-test_dict = {len(compressed_list[x]): compressed_list[x] for x in range(len(compressed_list))}
 
-print(compressed_list)
-print(test_dict)
+for i in compressed_list:
+    print(i)
