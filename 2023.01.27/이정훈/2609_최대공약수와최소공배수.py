ed04fc5aff7f25731d5c4efa06e7b60ac6763b8a
cocosasa <wjdgns0631@gmail.com>
2023-01-30 23:03:14 +0900

    commit!

--- 2023.01.27/이정훈/2609_최대공약수와최소공배수.py
+++ 2023.01.27/이정훈/2609_최대공약수와최소공배수.py
@@ -5,7 +5,7 @@ min_multiple = N*M
 for i in range(1,max(N,M)+1) :
 	if N % i == 0 and M % i == 0 and i > max_divisor :
 		max_divisor = i
-for i in range(min(N,M),N*M+1,min(N,M)) :
+for i in range(n(N,M),N*M+1,min(N,M)) :
 	if i % N == 0 and i % M == 0 :
 		min_multiple = i 
 		break
