cbcd7bf54214fd4db7ca9554bafe17a35c75ce80
이정훈 <wjdgns0631@gmail.com>
2023-02-21 14:50:11 +0900

    After

--- 2023.02.21/이정훈/16922_로마숫자만들기.py
+++ 2023.02.21/이정훈/16922_로마숫자만들기.py
@@ -3,14 +3,17 @@ import itertools
 N = int(input())
 roma_val = {'I':1,'V':5,'X':10,'L':50}
 
+#중복 제거를 위한 Set
 ans = set()
+# 중복 조합
 combination = itertools.combinations_with_replacement(roma_val.keys(), N)
 for i in combination:
+    # dict의 value로 매핑해서 sum
     ans.add(sum(map(lambda x: roma_val[x],i)))
 print(len(ans))
 
 
-
+# 중복순열 - 시간초과
 # from itertools import product
 
 # N = int(input())
--- 2023.02.21/이정훈/2992_크면서작은수.py
+++ 2023.02.21/이정훈/2992_크면서작은수.py
@@ -2,9 +2,11 @@ from itertools import permutations
 
 n = input()
 m = sorted(list(n))
+# 정렬된 수의 리스트로 순열을 만들면 값이 작은 순열부터 나옴
 p = map(lambda x : int(''.join(x)),permutations(m, len(n)))
 
 n = int(n)
+# 큰 값이 존재하지 않을 경우
 ans = 0
 for num in p :
     if n < num:
