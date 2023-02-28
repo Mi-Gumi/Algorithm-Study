3c9af15756bb74e069e4db0b94da1a9066389455
cocosasa <wjdgns0631@gmail.com>
2023-02-28 20:00:09 +0900

    buzzer beater.

--- 2023.02.28/이정훈/9251_LCS.py
+++ 2023.02.28/이정훈/9251_LCS.py
@@ -2,6 +2,23 @@
 # 주어졌을 때, 모두의 부분 수열이 되는 수열 중 가장 긴 것을 찾는 문제이다.
 # 예를 들어, ACAYKP와 CAPCAK의 LCS는 ACAK가 된다.
 
+A = input()
+B = input()
+A = '0' + A
+B = '0' + B
+len_A = len(A)
+len_B = len(B)
+LCS = [[0]*(len_B) for _ in range(len_A)]
+
+for i in range(len_A) :
+    for j in range(len_B) :
+        if i == 0 or j == 0:
+            LCS[i][j] = 0
+        elif A[i] == B[j]:
+            LCS[i][j] = LCS[i - 1][j - 1] + 1
+        else:
+            LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1])
+print(LCS[-1][-1])
 
 ##############################################################################
 # 공통 부분만으로 제일 긴 길이를 가진 조합부터 찾다가 결과가 나오면 측시 break  => 메모리 초과
