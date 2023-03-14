2392b80af4f8281b9c3641b0d4e5505137f6518c
rlvy98 <rlvy98@naver.com>
2023-03-14 17:38:44 +0900

    0314

--- 2023.03.14/이기표/14502_연구소.py
+++ 2023.03.14/이기표/14502_연구소.py
@@ -7,7 +7,7 @@ from itertools import combinations
 from collections import deque
 import copy
 
-def bfs(i, j):
+def bfs(i, j): # bfs를 통해 바이러스 감염
     que = deque()
     que.append((i, j))
 
@@ -27,8 +27,8 @@ def bfs(i, j):
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-wall = []
-covid = []
+wall = [] # 벽이 위치할 수 있는 리스트
+covid = [] # 현재 바이러스가 있는 위치
 for i in range(N):
     for j in range(M):
         if arr[i][j] == 0:
@@ -41,10 +41,10 @@ for c in combinations(wall, 3):
     new_arr = copy.deepcopy(arr)
     for x, y in c: # 벽 세우기
         new_arr[x][y] = 1
-    for lst in covid: # 바이러스
+    for lst in covid: # 바이러스 감염
         bfs(lst[0], lst[1])
     cnt = 0
-    for lst in new_arr:
+    for lst in new_arr: # 감염 후 안전 구역 찾기
         cnt += lst.count(0)
     max_cnt = max(max_cnt, cnt)
 print(max_cnt)
--- 2023.03.14/이기표/2580_스도쿠.py
+++ 2023.03.14/이기표/2580_스도쿠.py
@@ -6,7 +6,7 @@ arr = [list(map(int, input().split())) for _ in range(9)]
 zero_li = deque()
 for i in range(9):
     for j in range(9):
-        if arr[i][j] == 0:
+        if arr[i][j] == 0: # 0의 위치를 리스트화
             zero_li.append((i, j))
 
 def check_num(si, sj, num):
@@ -17,7 +17,7 @@ def check_num(si, sj, num):
     else:
         ci = si//3 * 3
         cj = sj//3 * 3
-        for i in range(ci, ci+3):
+        for i in range(ci, ci+3): # 사각형 탐색
             for j in range(cj, cj+3):
                 if arr[i][j] == num:
                     return False
@@ -25,15 +25,16 @@ def check_num(si, sj, num):
 
 def search(idx):
     if idx == len(zero_li):
-        for lst in arr:
+        for lst in arr: # 결과 도출
             print(' '.join(map(str, lst)))
         sys.exit(0)
 
     i, j = zero_li[idx]
+    # 0의 위치에 들어갈 수 있는 1부터 9까지의 모든 경우를 시도
     for n in range(1, 10):
-        if check_num(i, j, n):
+        if check_num(i, j, n): # 이미 행 열 사각에 그 값이 존재하는지 확인
             arr[i][j] = n
-            search(idx+1)
+            search(idx+1) # 재귀탐색
             arr[i][j] = 0
 
 search(0)
