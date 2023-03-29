b2e9b4c414845fbfc2e746f1c38066d067d8f356
leejeonghun <wjdgns0631@gmail.com>
2023-03-29 17:30:47 +0900

    pipe install

--- 2023.03.28/이정훈/17070_파이프옮기기.py
+++ 2023.03.28/이정훈/17070_파이프옮기기.py
@@ -1,7 +1,7 @@
 def move(ci, cj, dr) :
     if ci == N-1 and cj == N-1 :
         return 1
-    possible = 0
+    memo[ci][cj][dr] = 0
     for di, dj, chk, ndr in dr_d[dr] :
         for chk_i, chk_j in chk :
             nchk_i = ci + chk_i
@@ -12,33 +12,18 @@ def move(ci, cj, dr) :
                 break
         else :
             ni, nj = ci + di , cj + dj
-    
-            if ndr == 0 :
-                if memo[ni][nj][0] >= 0 and memo[ni][nj][1] >= 0 :
-                    tmp = memo[ni][nj][0] + memo[ni][nj][1]
-                else :
-                    tmp = move(ni, nj, ndr )
-            elif ndr == 1 :
-                if memo[ni][nj][0] >= 0 and memo[ni][nj][1] >= 0 and memo[ni][nj][1] >= 0:
-                    tmp = memo[ni][nj][0] + memo[ni][nj][1] + memo[ni][nj][2]
-                else :
-                    tmp = move(ni, nj, ndr )
-            elif ndr == 2 :
-                if memo[ni][nj][1] >= 0 and memo[ni][nj][1] >= 0:
-                    tmp = memo[ni][nj][1] + memo[ni][nj][2]
-                else :
-                    tmp = move(ni, nj, ndr )
-            
-            memo[ni][nj][ndr] = tmp
-            possible += tmp
+            if memo[ni][nj][ndr] >= 0 :
+                tmp = memo[ni][nj][ndr]
+            else :
+                tmp = move(ni,nj,ndr)
+            memo[ci][cj][dr] += tmp
 
-    return possible
+    return memo[ci][cj][dr]
 
 N = int(input())
 
 state = [list(map(int,input().split())) for _ in range(N)]
 
-d = ((0,1,((0,1)), 0), (1,1,((0,1),(1,0),(1,1)),1),(1,0,((1,0),)),2)
 dr_d = {
     0 : ((0,1,((0,1),), 0), (1,1,((0,1),(1,0),(1,1)), 1)),
     1 : ((0,1,((0,1),), 0), (1,1,((0,1),(1,0),(1,1)), 1),(1,0,((1,0),), 2)),
@@ -46,12 +31,7 @@ dr_d = {
 }
 
 dr = 0
-memo = [[[-1]*3]*N for _ in range(N)] 
-state[0][0] = 2
-state[0][1] = 2
-
-head_i = 0
-head_j = 1
+memo = [[[-1]*3 for _ in range(N)] for _ in range(N)] 
 
 ans = move(0,1,0)
 
