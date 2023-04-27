50542df9a05538b5b75953af16f7150809ec3d7a
cocosasa <wjdgns0631@gmail.com>
2023-04-27 19:20:36 +0900

    gogo

--- 2023.04.27/이정훈/1541_잃어버린괄호.py
+++ 2023.04.27/이정훈/1541_잃어버린괄호.py
@@ -1,3 +1,13 @@
 formula = input()
+# -로 나누고 합한 연산을 차례로 - 연산
+split_minus = formula.split('-')
 
-# 괄호
+# print(split_minus)
+arr = []
+for plus in split_minus :
+    arr.append(list(map(int,plus.split('+'))))
+# print(arr)
+ans = sum(arr[0])
+for i in range(1, len(arr)) :
+    ans -= sum(arr[i])
+print(ans)
